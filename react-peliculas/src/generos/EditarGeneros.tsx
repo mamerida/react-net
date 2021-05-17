6318fd4b1e641dc9d00a71817b4d285b7d0a0667
mamerida <mamerida2018@gmail.com>
2021-05-17 13:17:09 -0400

    create 404 cath

--- react-peliculas/src/generos/EditarGeneros.tsx
+++ react-peliculas/src/generos/EditarGeneros.tsx
@@ -1,5 +1,11 @@
+import { useParams } from "react-router-dom"
 export default function EditarGenero(){
+    const {id}:any = useParams()
     return(
-        <>Editar Genero</>
+        <>
+        <h3>Editar Genero</h3>
+        <h4>El id es {id}</h4>
+        
+        </>
     )
 }
--- react-peliculas/src/route-config.ts
+++ react-peliculas/src/route-config.ts
@@ -17,27 +17,34 @@ import FiltroPeliculas from './peliculas/FiltroPeliculas'
 import CrearPeliculas from './peliculas/CrearPeliculas'
 import EditarPeliculas from './peliculas/EditarPeliculas'
 
+import RedirectALanding from './utils/RedirectALanding'
+
 
 //para evitar tener todas las rutas en el main  directamente las puedo configurar aca 
+//para poder extraer datos de la url tengo que asignarles una variable de ruta 
+///:id(\\d+) indico que id va a ser numerico esto lo utilizo para leer la variable de ruta y usarlo en otros 
+//componentes
 const rutas = [
     {path:'/generos',componente: IndiceGeneros, exact:true},
     {path:'/generos/crear',componente: CrearGeneros},
-    {path:'/generos/editar',componente: EditarGenero},
+    {path:'/generos/editar/:id(\\d+)',componente: EditarGenero},
 
     {path:'/actores',componente: IndiceActores, exact:true},
     {path:'/actores/crear',componente: CrearActores},
-    {path:'/actores/editar',componente: EditarActores},
+    {path:'/actores/editar/:id(\\d+)',componente: EditarActores},
 
     {path:'/cines',componente: IndiceCines, exact:true},
     {path:'/cines/crear',componente: CrearCines},
-    {path:'/cines/editar',componente: EditarCines},
+    {path:'/cines/editar/:id(\\d+)',componente: EditarCines},
 
 
     {path:'/peliculas/filtrar',componente: FiltroPeliculas},
     {path:'/peliculas/crear',componente: CrearPeliculas},
-    {path:'/peliculas/editar',componente: EditarPeliculas},
+    {path:'/peliculas/editar/:id(\\d+)',componente: EditarPeliculas},
 
-    {path :'/',componente :LandingPage, exact : true }
+    {path :'/',componente :LandingPage, exact : true },
+    //para evitar pantallas en blanco cuando no entiende la ruta nos vamos a colocar en un pad generico
+    {path :"*", componente: RedirectALanding }
 
 
 ];
