39519f4d3bd0a18c98b29d6b160b0a7a045e25f8
mamerida <mamerida2018@gmail.com>
2021-05-17 12:51:53 -0400

    create a button for surf between pages

--- react-peliculas/src/generos/CrearGeneros.tsx
+++ react-peliculas/src/generos/CrearGeneros.tsx
@@ -1,5 +1,15 @@
+import { useHistory } from "react-router-dom"
+import Button from "../utils/Button";
+
 export default function CrearGenero(){
+    // Navegar por el usuario con un useHistory
+    const history = useHistory();
     return(
-        <>Crear Genero</>
+
+        <>
+        <h3>Crear Genero</h3>
+        <Button onClick ={() => history.push('/generos')}>Salvar</Button>
+        </>
+        //  history.push('/generos') puedo decirle a donde quiero que navege el usuario indicando el path
     )
 }
--- react-peliculas/src/utils/Button.tsx
+++ react-peliculas/src/utils/Button.tsx
@@ -4,12 +4,17 @@ export default function Button (props : ButtonProps ){
 
     return(
 
-        <button type="button" className="btn btn-primary">{props.children} </button>
+        <button type="button"
+         className="btn btn-primary"
+        onClick={props.onClick}
+        >{props.children} </button>
 
     )
 
 }
 
 interface ButtonProps{
-    children:React.ReactNode
+    children:React.ReactNode,
+    // defino un onCLick para poder darle funcionalidad a los botones
+    onClick():void
 }
